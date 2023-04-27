eecd85a4a10b986c6bdc9fdf8c544b9ae111c74a
Robin Simonneau <robin.simonneau@robin-sim.fr>
2023-04-27 11:32:20 +0200

    Ajout de la méthode more_userdata_istep_menu_content qui s'occupe d'afficher le contenue de la page d'administration dédié

--- index.php
+++ index.php
@@ -75,3 +75,42 @@ function more_userdata_istep_menu(): void{
     );
 }
 add_action( 'admin_menu', 'more_userdata_istep_menu' );
+
+/**
+ * Gère le contenue de la page administrateur
+ * @return void
+ */
+function more_userdata_istep_menu_content(): void {
+    // Vérifie si le formulaire a été soumis
+    if (isset($_POST['submit'])) {
+        // Met à jour les options avec les rôles sélectionnés
+        update_option('istep_user_roles', $_POST['istep_user_roles']);
+        echo '<div id="message" class="updated notice"><p>Rôles mis à jour avec succès.</p></div>';
+    }
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form method="post" action="">
+            <?php wp_nonce_field( 'istep_user_roles_nonce', 'istep_user_roles_nonce' ); ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="istep_user_roles"><?php _e( 'Rôles:', 'istep_users' ); ?></label></th>
+                    <td>
+                        <?php
+                        // Récupère tous les rôles WordPress
+                        $roles = wp_roles()->get_names();
+                        // Récupère les rôles sélectionnés dans la base de données
+                        $selected_roles = get_option('istep_user_roles', array());
+                        // Affiche une checkbox pour chaque rôle
+                        foreach ($roles as $key => $value) {
+                            echo '<label><input type="checkbox" name="istep_user_roles[]" value="'.$key.'" '.checked(in_array($key, $selected_roles), true, false).'>'.$value.'</label><br/>';
+                        }
+                        ?>
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button('Enregistrer les rôles', 'primary', 'submit', true); ?>
+        </form>
+    </div>
+    <?php
+}
