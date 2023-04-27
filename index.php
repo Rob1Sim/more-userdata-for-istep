a714d03fcab65cea9d80d662699dc196413a1966
Robin Simonneau <robin.simonneau@robin-sim.fr>
2023-04-27 14:56:18 +0200

    Ajout d'un sous menu dans le panel administrateur qui permet d'ajouter des équipes

--- index.php
+++ index.php
@@ -145,6 +145,14 @@ function more_userdata_istep_menu(): void{
         "istep_users_options",
         "more_userdata_istep_menu_content"
     );
+    add_submenu_page(
+        'istep_users_options', // slug du parent
+        'Gérer les équipes', // titre de la page
+        'Gérer les équipes', // titre du menu
+        'manage_options', // capacité requise
+        'istep_manage_teams', // slug de la page
+        'more_userdata_istep_menu_team_page' // fonction de rappel
+    );
 }
 add_action( 'admin_menu', 'more_userdata_istep_menu' );
 
@@ -187,3 +195,65 @@ function more_userdata_istep_menu_content(): void {
     </div>
     <?php
 }
+
+function more_userdata_istep_menu_team_page() {
+    // Vérifie si le formulaire a été soumis
+    if (isset($_POST['submit'])) {
+        // Ajoute une nouvelle équipe à la base de données
+        $nom_equipe = $_POST['nom_equipe'];
+        if (isset($nom_equipe)){
+            global $wpdb;
+            $wpdb->insert(
+                TABLE_TEAM_NAME,
+                array(
+                    'nom_equipe' => $nom_equipe
+                )
+            );
+            echo '<div id="message" class="updated notice"><p>Équipe ajoutée avec succès.</p></div>';
+        }
+        }
+    ?>
+    <div class="wrap">
+        <h1></h1>
+        <h2>Ajouter une nouvelle équipe</h2>
+        <form method="post" action="">
+            <?php wp_nonce_field( 'ajouter_equipe_nonce', 'ajouter_equipe_nonce' ); ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="nom_equipe"><?php _e( 'Nom de l\'équipe:', 'istep_users' ); ?></label></th>
+                    <td>
+                        <input type="text" name="nom_equipe" id="nom_equipe" value="">
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button('Ajouter', 'primary', 'submit', true); ?>
+        </form>
+        <hr>
+        <h2>Liste des équipes</h2>
+        <table class="wp-list-table widefat fixed striped">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nom de l'équipe</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            global $wpdb;
+            $table_name = TABLE_TEAM_NAME;
+            $equipes = $wpdb->get_results("SELECT * FROM $table_name");
+            foreach ($equipes as $equipe) {
+                echo '<tr>';
+                echo '<td>' . $equipe->id_equipe . '</td>';
+                echo '<td>' . $equipe->nom_equipe . '</td>';
+                echo '</tr>';
+            }
+            ?>
+            </tbody>
+        </table>
+    </div>
+    <?php
+}
+
+
+
