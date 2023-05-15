2e26bd97564c728024a809808f6482e9e1ce8976
Robin Simonneau <robin.simonneau@robin-sim.fr>
2023-05-15 11:06:58 +0200

    Ajout de la modification et de la suppression des campus

--- admin-functions.php
+++ admin-functions.php
@@ -23,6 +23,7 @@ function more_userdata_istep_menu(): void{
         'admin_users_options', // slug de la page
         'more_userdata_istep_menu_give_access' // fonction de rappel
     );
+    // --- Les équipes ---
     add_submenu_page(
         'istep_users_options', // slug du parent
         'Gérer les équipes', // titre de la page
@@ -30,22 +31,6 @@ function more_userdata_istep_menu(): void{
         ADMIN_CAPACITY, // capacité requise
         'istep_manage_teams', // slug de la page
         'more_userdata_istep_menu_team_page' // fonction de rappel
-    );
-    add_submenu_page(
-        'istep_users_options', // slug du parent
-        'Gérer les campus', // titre de la page
-        'Gérer les campus', // titre du menu
-        ADMIN_CAPACITY, // capacité requise
-        'istep_manage_location', // slug de la page
-        'more_userdata_istep_menu_location_page' // fonction de rappel
-    );
-    add_submenu_page(
-        'istep_users_options',
-        'Membres de l\'ISTeP',
-        'Membres de l\'ISTeP',
-        ADMIN_CAPACITY,
-        'istep_users_list',
-        'more_userdata_istep_users_list'
     );
     add_submenu_page(
         'admin.php?page=edit_teams&id=',
@@ -63,6 +48,15 @@ function more_userdata_istep_menu(): void{
         'delete_teams',
         'more_userdata_istep_delete_equipe_page'
     );
+    // --- Les utilisateurs ---
+    add_submenu_page(
+        'istep_users_options',
+        'Membres de l\'ISTeP',
+        'Membres de l\'ISTeP',
+        ADMIN_CAPACITY,
+        'istep_users_list',
+        'more_userdata_istep_users_list'
+    );
     add_submenu_page(
         'admin.php?page=modify_users_teams&id=',
         'Modifier équipe',
@@ -71,8 +65,31 @@ function more_userdata_istep_menu(): void{
         'modify_users_teams',
         'more_userdata_istep_users_edit_teams'
     );
-
-
+    // --- Les campus ---
+    add_submenu_page(
+        'istep_users_options', // slug du parent
+        'Gérer les campus', // titre de la page
+        'Gérer les campus', // titre du menu
+        ADMIN_CAPACITY, // capacité requise
+        'istep_manage_location', // slug de la page
+        'more_userdata_istep_menu_location_page' // fonction de rappel
+    );
+    add_submenu_page(
+        'admin.php?page=edit_location&id=',
+        'Modifier le campus',
+        'Modifier le campus',
+        ADMIN_CAPACITY,
+        'edit_location',
+        'more_userdata_istep_edit_location_page'
+    );
+    add_submenu_page(
+        'admin.php?page=suppress_location&id=',
+        'Supprimer campus',
+        'Supprimer campus',
+        ADMIN_CAPACITY,
+        'suppress_location',
+        'more_userdata_istep_delete_location_page'
+    );
 }
 add_action( 'admin_menu', 'more_userdata_istep_menu' );
 
@@ -379,7 +396,7 @@ function more_userdata_istep_delete_equipe_page() {
     }
     if ( current_user_can( ADMIN_CAPACITY ) && isset($_POST['equipe_id_delete']) ) {
         // Récupère l'ID de l'équipe à supprimer depuis l'URL
-        $id_equipe = $_POST['equipe_id_delete'];
+        $id_equipe = sanitize_text_field($_POST['equipe_id_delete']);
         // Supprime l'équipe de la base de données
         global $wpdb;
         $table_name = TABLE_TEAM_NAME;
@@ -389,7 +406,6 @@ function more_userdata_istep_delete_equipe_page() {
                 'id_equipe' => intval($id_equipe)
             )
         );
-        var_dump($wpdb->last_query);
         // Vérifie s'il reste des équipes dans la table
         $count = $wpdb->get_var("SELECT COUNT(*) FROM $table_name");
         if ($count == 0) {
@@ -603,7 +619,7 @@ function more_userdata_istep_menu_location_page(){
             <?php wp_nonce_field( 'ajouter_campus_nonce', 'ajouter_campus_nonce' ); ?>
             <table class="form-table">
                 <tr>
-                    <th scope="row"><label for="nom_campus"><?php _e( 'Nom de l\'équipe:', 'istep_users' ); ?></label></th>
+                    <th scope="row"><label for="nom_campus"><?php _e( 'Nom du campus:', 'istep_users' ); ?></label></th>
                     <td>
                         <input type="text" name="nom_localisation" id="nom_campus" value="" required>
                     </td>
@@ -628,13 +644,13 @@ function more_userdata_istep_menu_location_page(){
                 echo '<td>' . $location->id_localisation . '</td>';
                 echo '<td>' . $location->nom_localisation . '</td>';
                 echo '<td>
-                        <form method="post" action="' . admin_url( 'admin.php?page=edit_teams&id=' . $location->id_localisation ) . '">
-                            <input type="hidden" name="id" value="' . $location->id_localisation . '">
+                        <form method="post" action="' . admin_url( 'admin.php?page=edit_location&id=' . $location->id_localisation ) . '">
+                            <input type="hidden" name="id" value="' . $location->nom_localisation . '">
                             <button type="submit" class="button">Modifier</button>
                         </form>
                       </td>';
                 echo '<td>
-                        <form method="post" action="' . admin_url( 'admin.php?page=delete_teams&id=' . $location->id_localisation ) . '">
+                        <form method="post" action="' . admin_url( 'admin.php?page=suppress_location&id=' . $location->id_localisation ) . '">
                             <input type="hidden" name="location_id_delete" value="' . $location->id_localisation . '">
                             <button type="submit" class="button">Supprimer</button>
                         </form>
@@ -648,7 +664,101 @@ function more_userdata_istep_menu_location_page(){
     <?php
 }
 
+/**
+ * Modifie les informations d'un campus
+ * @return void
+ */
+function more_userdata_istep_edit_location_page() {
+    if ( !can_user_access_this(get_option('admin_user_roles')) ) {
+        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+    global $wpdb;
 
+    $id_location = $_GET['id'];
+
+    if (isset($_POST['submit']) && isset($id_location)) {
+        if ( current_user_can( ADMIN_CAPACITY ) ) {
+
+            $location_name = sanitize_text_field($_POST['nom_location']);
+            if (isset($location_name)){
+                $wpdb->update(
+                    TABLE_LOCATION_NAME,
+                    array(
+                        'nom_localisation' => $location_name
+                    ),
+                    array(
+                        'id_localisation' => $id_location
+                    )
+                );
+                echo '<div id="message" class="updated notice"><p>Campus modifiée avec succès.</p></div>';
+            }
+        } else {
+            echo '<div id="message" class="notice notice-error"><p>Vous n\'avez pas la permission de faire ça.</p></div>';
+        }
+    }
+
+
+    $table_name = TABLE_LOCATION_NAME;
+    $location = $wpdb->get_row("SELECT * FROM $table_name WHERE id_localisation = $id_location");
+
+    ?>
+    <div class="wrap">
+        <h1>Modifier le campus <?php echo $location->nom_localisation; ?></h1>
+        <form method="post" action="">
+            <?php wp_nonce_field( 'modifier_location_nonce', 'modifier_location_nonce' ); ?>
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="nom_equipe"><?php _e( 'Nom du campus:', 'istep_users' ); ?></label></th>
+                    <td>
+                        <input type="text" name="nom_location" id="nom_equipe" value="<?php echo $location->nom_localisation; ?>">
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button('Enregistrer', 'primary', 'submit', true); ?>
+        </form>
+    </div>
+    <?php
+}
+
+/**
+ * Supprime de la bd le campus avec l'id correspondant
+ * @return void
+ */
+function more_userdata_istep_delete_location_page() {
+    if ( !can_user_access_this(get_option('admin_user_roles')) ) {
+        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    }
+    if ( current_user_can( ADMIN_CAPACITY ) && isset($_POST['location_id_delete']) ) {
+
+        $id_location = sanitize_text_field($_POST['location_id_delete']);
+
+        global $wpdb;
+        $table_name = TABLE_LOCATION_NAME;
+        $wpdb->delete(
+            $table_name,
+            array(
+                'id_localisation' => intval($id_location)
+            )
+        );
+        // Vérifie s'il reste des équipes dans la table
+        $count = $wpdb->get_var("SELECT COUNT(*) FROM $table_name");
+        if ($count == 0) {
+
+            $wpdb->insert(
+                $table_name,
+                array(
+                    'nom_localisation' => 'Sorbonne Université - Campus Pierre et Marie Curie',
+                )
+            );
+        }
+
+        echo '<div id="message" class="updated notice"><p>Campus supprimée avec succès.</p></div>';
+    } else {
+        echo '<div id="message" class="notice notice-error"><p>Vous n\'avez pas la permission de faire ça.</p></div>';
+
+    }
+
+}
 /**
  * Ajoute a l'option passé en paramètre l'administrateur
  * @param string $option_name
