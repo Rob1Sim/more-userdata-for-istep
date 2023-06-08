cf573211b200a1396deb108b87b3d287539f9e16
Robin Simonneau <robin.le.dev@gmail.com>
2023-06-08 13:18:03 +0200

    Modification de users pour correspondre à l'orienté objet

--- scripts/admin/users.php
+++ scripts/admin/users.php
@@ -3,8 +3,9 @@
  * Gestion de la listes des utilisateur
  */
 
-
-
+use MUDF_ISTEP\Entity\Member;
+use MUDF_ISTEP\Entity\Location;
+use MUDF_ISTEP\Entity\Team;
 
 wp_enqueue_script('more-userdata-for-istep-admin-js', plugins_url('../../public/scripts/more-userdata-for-istep-admin.js', __FILE__), array(), false, true);
 
@@ -61,41 +62,51 @@ function more_userdata_istep_users_list():void
             </thead>
             <tbody>
 <?php
-                $users = get_list_of_table(TABLE_MEMBERS_NAME);
+                $users = Member::getAll();
     foreach ($users as $user) {
-        $teams = get_user_teams_names_by_user_id($user->id_membre);
-        $wp_user = get_userdata($user->wp_user_id);
-        echo '<tr>';
-        echo '<td>' . $user->id_membre . '</td>';
-        echo '<td>' . $wp_user->display_name . '</td>';
-        echo '<td>' . $wp_user->user_login . '</td>';
-        echo '<td>';
-        foreach ($teams as $team) {
-            echo $team.', ';
-        }
-        echo '</td>';
-        echo '<td>' . $user->fonction . '</td>';
-        echo '<td>' . $wp_user->user_email . '</td>';
-        echo '<td>' . $user->nTelephone . '</td>';
-        echo '<td>' . $user->rangEquipe . '</td>';
-        echo '<td>' . convert_tower_into_readable($user->tourDuBureau) . '</td>';
-        echo '<td>' . $user->bureau . '</td>';
-        echo '<td>' . get_name_of_location_by_id(intval($user->campus)) . '</td>';
-        echo '<td>' . $user->employeur . '</td>';
-        echo '<td>' . $user->caseCourrier . '</td>';
-        echo '<td>
-                        <form method="post" action="' . admin_url('admin.php?page=modify_users_data&id=' . $user->id_membre) . '">
-                            <input type="hidden" name="id" value="' . $user->id_membre . '">
+        try {
+            $member = Member::findById($user->id_membre);
+            $teams = $member->getTeamsNames();
+            $wp_user = $member->getWPUser();
+            echo '<tr>';
+            echo '<td>' . $member->getId() . '</td>';
+            echo '<td>' . $wp_user->display_name . '</td>';
+            echo '<td>' . $wp_user->user_login . '</td>';
+            echo '<td>';
+            foreach ($teams as $team) {
+                echo $team.', ';
+            }
+            echo '</td>';
+            echo '<td>' . $member->getFunction() . '</td>';
+            echo '<td>' . $wp_user->user_email . '</td>';
+            echo '<td>' . $member->getPhone() . '</td>';
+            echo '<td>' . $member->getTeamRank() . '</td>';
+            echo '<td>' . $member->getReadableOfficeTower() . '</td>';
+            echo '<td>' . $member->getOffice() . '</td>';
+            try{
+                echo '<td>' . $member->getLocation()->getName() . '</td>';
+            }catch (\MUDF_ISTEP\Exception\EntityNotFound $e) {
+                echo '<td>Pas de campus </td>';
+            }
+            echo '<td>' . $member->getEmployer() . '</td>';
+            echo '<td>' . $member->getMailCase() . '</td>';
+            echo '<td>
+                        <form method="post" action="' . admin_url('admin.php?page=modify_users_data&id=' . $member->getId()) . '">
+                            <input type="hidden" name="id" value="' . $member->getId() . '">
                             <button type="submit" class="button">Modifier</button>
                         </form>
                       </td>';
-        echo '<td>
-                        <form method="post" action="' . admin_url('admin.php?page=erase_user&id=' . $user->id_membre) . '">
-                            <input type="hidden" name="user_delete_id" value="' . $user->id_membre . '">
+            echo '<td>
+                        <form method="post" action="' . admin_url('admin.php?page=erase_user&id=' . $member->getId()) . '">
+                            <input type="hidden" name="user_delete_id" value="' . $member->getId() . '">
                             <button type="submit" class="button button-primary" style="background: #d0021b; border-color: #d0021b">Supprimer</button>
                         </form>
                       </td>';
-        echo '</tr>';
+            echo '</tr>';
+        } catch (\MUDF_ISTEP\Exception\InvalidParameter|\MUDF_ISTEP\Exception\MemberNotFound $e) {
+            echo '<div class="notice notice-error"><p>Une erreur est survenue</p></div>';
+        }
+
     }
     ?></tbody>
         </table>
@@ -129,7 +140,7 @@ function more_userdata_istep_users_edit_data():void
             && isset($employer) && isset($mailCase) && isset($campus) && isset($office_tower) && isset($office) && isset($phone)) {
 
             //Vérification du campus
-            if(!is_location_existing($campus)) {
+            if(!Location::is_location($campus)) {
                 echo '<div id="message" class="notice notice-error">Le campus n\'existe pas !</div>';
                 edit_user_form();
                 exit();
@@ -149,22 +160,11 @@ function more_userdata_istep_users_edit_data():void
                 exit();
             }
             //Mis à jour de l'utilisateur
-            global $wpdb;
-            $wpdb->update(TABLE_MEMBERS_NAME, array(
-                "fonction"=>$function,
-                "caseCourrier"=>$mailCase,
-                "employeur"=>$employer,
-                "rangEquipe"=>$rank,
-                "nTelephone" => $phone,
-                "tourDuBureau" => $office_tower,
-                "bureau"=>$office,
-                "campus_location"=>$campus
-            ), array(
-                "wp_user_id"=>$wp_id
-            ));
-            $display_name = $last_name ." ".$first_name;
-            $user_data = get_userdata($wp_id);
+            $new_member = new Member($id_user,$wp_id,$campus,$function,$phone,$office,$office_tower,$employer,$mailCase,$rank);
+            $new_member->update();
 
+            $display_name = $last_name ." ".$first_name;
+            $user_data = $new_member->getWPUser();
             $user_data->display_name = $display_name;
 
             $result = wp_update_user($user_data);
@@ -181,34 +181,38 @@ function more_userdata_istep_users_edit_data():void
         }
         //Vérification des équipes
         $verified_teams = [];
-        $already_exist_data= get_user_teams_id_by_user_id($id_user);
-        $teams_already_in = [];
-        if (!isset($_POST['teams'])) {
-            $verified_teams[] = 1;
-        } else {
-            $teams = $_POST['teams'];
-            foreach ($teams as $team) {
-                //Si l'équipe n'éxiste pas on l'ajoute
-                if (!in_array($team, $already_exist_data)) {
-                    $verified_teams[] = sanitize_text_field($team);
-                } else {
-                    $teams_already_in[] = sanitize_text_field($team);
+        try {
+            $already_exist_data = Member::findById($id_user)->getTeams();
+            $teams_already_in = [];
+            if (!isset($_POST['teams'])) {
+                $verified_teams[] = 1;
+            } else {
+                $teams = $_POST['teams'];
+                foreach ($teams as $team) {
+                    //Si l'équipe n'éxiste pas on l'ajoute
+                    if (!in_array($team, $already_exist_data)) {
+                        $verified_teams[] = sanitize_text_field($team);
+                    } else {
+                        $teams_already_in[] = sanitize_text_field($team);
+                    }
                 }
             }
+
+            //On récupère les tables qui n'était pas coché mais présent dans les tables de l'utilisateur
+            $teams_to_delete = array_diff($already_exist_data, $teams_already_in);
+            foreach ($teams_to_delete as $team) {
+                //on les supprime
+                delete_data_from_team_members($team, sanitize_text_field($id_user));
+            }
+            //on ajoute les nouvelle
+            add_data_to_team_members($verified_teams, $id_user);
+
+
+            echo '<div id="message" class="updated notice"><p>Mis à jour réalisé avec succès.</p></div>';
+            echo '<a href="'.admin_url("admin.php?page=istep_users_list").'">Retour à la liste</a>';
+        } catch (\MUDF_ISTEP\Exception\InvalidParameter|\MUDF_ISTEP\Exception\MemberNotFound $e) {
+            echo '<div class="notice notice-error"><p>Une erreur est survenue</p></div>';
         }
-
-        //On récupère les tables qui n'était pas coché mais présent dans les tables de l'utilisateur
-        $teams_to_delete = array_diff($already_exist_data, $teams_already_in);
-        foreach ($teams_to_delete as $team) {
-            //on les supprime
-            delete_data_from_team_members($team, sanitize_text_field($id_user));
-        }
-        //on ajoute les nouvelle
-        add_data_to_team_members($verified_teams, $id_user);
-
-
-        echo '<div id="message" class="updated notice"><p>Mis à jour réalisé avec succès.</p></div>';
-        echo '<a href="'.admin_url("admin.php?page=istep_users_list").'">Retour à la liste</a>';
     } else {
         edit_user_form();
     }
@@ -227,14 +231,18 @@ function more_userdata_istep_users_delete_user():void
     }
     if (current_user_can(ADMIN_CAPACITY) && isset($_POST['user_delete_id'])) {
         $id = sanitize_text_field($_POST['user_delete_id']);
-        $wp_user = get_wp_user_from_istep_user($id);
-        if($wp_user !== false) {
-            wp_delete_user($wp_user->ID);
-            echo '<div id="message" class="updated notice"><p>L\'utilisateur à été supprimé avec succès.</p></div>';
-        } else {
-            echo '<div class="notice notice-error"><p>Une erreur est survenue lors de la suppression</p></div>';
+        try {
+            $wp_user = Member::findById($id)->getWPUser();
+            if($wp_user !== false) {
+                wp_delete_user($wp_user->ID);
+                echo '<div id="message" class="updated notice"><p>L\'utilisateur à été supprimé avec succès.</p></div>';
+            } else {
+                echo '<div class="notice notice-error"><p>Une erreur est survenue lors de la suppression</p></div>';
+            }
+            delete_personal_page_by_wp_user($wp_user);
+        } catch (\MUDF_ISTEP\Exception\InvalidParameter|\MUDF_ISTEP\Exception\MemberNotFound $e) {
+            echo '<div class="notice notice-error"><p>Une erreur est survenue</p></div>';
         }
-        delete_personal_page_by_wp_user($wp_user);
     }
 }
 
@@ -247,79 +255,84 @@ function edit_user_form():void
     if (isset($_POST['id']) || isset($_GET["id"])) {
         $id_user = sanitize_text_field($_POST['id'] ?? $_GET["id"]);
 
-        $teams = get_list_of_table(TABLE_TEAM_NAME);
-        $users_teams = get_user_teams_id_by_user_id($id_user);
+        $teams = Team::getAll();
+        try {
+            $user_data = Member::findById($id_user);
+            $users_teams = $user_data->getTeams();
+            $user_data_wp = get_user_by('id', $user_data->getWpId());
 
-        $user_data = get_istep_user_by_id($id_user, "istep");
-        $user_data_wp = get_user_by('id', $user_data->wp_user_id);
+            $display_name = explode(" ", $user_data_wp->display_name);
+            $last_name = $display_name[0] ?? "";
+            $first_name = $display_name[1] ?? "";
 
-        $display_name = explode(" ", $user_data_wp->display_name);
-        $last_name = $display_name[0] ?? "";
-        $first_name = $display_name[1] ?? "";
+            echo "<div class='update-user'>";
+            echo "<h1>Modifier les informations d'un utilisateur</h1>";
+            echo "<form method='POST' action=''>";
+            echo '<div><label for="last_name" >Nom de famille : </label><input type="text" name="last_name" id="last_name" value="'.$last_name.'" required></div>';
+            echo '<div><label for="first_name" >Prénom : </label><input type="text" name="first_name"  id="first_name" value="'.$first_name.'" required></div>';
 
-        echo "<div class='update-user'>";
-        echo "<h1>Modifier les informations d'un utilisateur</h1>";
-        echo "<form method='POST' action=''>";
-        echo '<div><label for="last_name" >Nom de famille : </label><input type="text" name="last_name" id="last_name" value="'.$last_name.'" required></div>';
-        echo '<div><label for="first_name" >Prénom : </label><input type="text" name="first_name"  id="first_name" value="'.$first_name.'" required></div>';
+            echo '<div><label for="phone" >Numéro de téléphone : </label><input type="tel" name="phone" id="phone" value="'.($user_data->getPhone() ?? "").'" required></div>';
 
-        echo '<div><label for="phone" >Numéro de téléphone : </label><input type="tel" name="phone" id="phone" value="'.($user_data->nTelephone ?? "").'" required></div>';
+            echo '<div><label for="office" >Bureau : </label><input type="text" name="office" id="office" value="'.($user_data->getOffice() ?? "").'" required></div>';
+            echo '<div><label for="function" >Fonction : </label><input type="text" name="function" id="function" value="'.($user_data->getFunction() ?? "").'" required></div>';
 
-        echo '<div><label for="office" >Bureau : </label><input type="text" name="office" id="office" value="'.($user_data->bureau ?? "").'" required></div>';
-        echo '<div><label for="function" >Fonction : </label><input type="text" name="function" id="function" value="'.($user_data->fonction ?? "").'" required></div>';
+            echo '<div><label for="tower" id="tower">Tour du bureau : <ul>';
 
-        echo '<div><label for="tower" id="tower">Tour du bureau : <ul>';
-
-        //Liste des tours
-        $towerList = ["tour-46-00-2e","tour-46-00-3e","tour-46-00-4e","tour-46-45-2e","tour-56-66-5e","tour-56-55-5e"];
-        foreach($towerList as $tower) {
-            echo '<li><label></label><input type="radio" name="tourBureau" value="'.$tower.'"'.($tower == $user_data->tourDuBureau ? "checked":""). ' />' .convert_tower_into_readable($tower).'</label> </li>';
-        }
-        echo ' </ul></label></div>';
+            //Liste des tours
+            $towerList = ["tour-46-00-2e","tour-46-00-3e","tour-46-00-4e","tour-46-45-2e","tour-56-66-5e","tour-56-55-5e"];
+            foreach($towerList as $tower) {
+                echo '<li><label></label><input type="radio" name="tourBureau" value="'.$tower.'"'.($tower == $user_data->getOfficeTower() ? "checked":""). ' />' .$user_data->getReadableOfficeTower().'</label> </li>';
+            }
+            echo ' </ul></label></div>';
 
 
-        //Listes des équipes
-        echo "<h2>Listes des équipes</h2>";
-        echo "<table class=\"form-table\">";
-        echo '<tr><th>Nom de l\'équipe</th><th>Fait partie de</th></tr>';
-        foreach ($teams as $team) {
+            //Listes des équipes
+            echo "<h2>Listes des équipes</h2>";
+            echo "<table class=\"form-table\">";
+            echo '<tr><th>Nom de l\'équipe</th><th>Fait partie de</th></tr>';
+            foreach ($teams as $team) {
 
-            $team_name = $team->nom_equipe;
-            $team_id = $team->id_equipe;
+                $team_name = $team->getName();
+                $team_id = $team->getId();
 
-            //Coche toute les équipes dont l'utilisateur fait déjà partie
-            $checked = (in_array($team_id, $users_teams))? "checked":"";
+                //Coche toute les équipes dont l'utilisateur fait déjà partie
+                $checked = (in_array($team_id, $users_teams))? "checked":"";
 
-            echo '<tr><td>'.$team_name.'</td><td><input type="checkbox" name="teams[]" value="'.$team_id.'"'.$checked.' id="team-'.$team_id.'"></td></tr>';
-        }
-        echo "</table>";
+                echo '<tr><td>'.$team_name.'</td><td><input type="checkbox" name="teams[]" value="'.$team_id.'"'.$checked.' id="team-'.$team_id.'"></td></tr>';
+            }
+            echo "</table>";
 
-        echo '<div><label for="rank" >Rang dans l\'équipe : </label><input type="text" id="rank" name="rank" value="'.($user_data->rangEquipe ?? "").'" required></div>';
+            echo '<div><label for="rank" >Rang dans l\'équipe : </label><input type="text" id="rank" name="rank" value="'.($user_data->getTeamRank() ?? "").'" required></div>';
 
-        //Listes de campus
-        echo '<div><label for="campus" >Campus : <select name="campus" id="campus">';
-        $campus = get_list_of_table(TABLE_LOCATION_NAME);
-        foreach ($campus as $one_campus) {
-            echo "<option value=\"".$one_campus->id_localisation."\">".$one_campus->nom_localisation."</option>";
-        }
-        echo '</select></label></div>';
+            //Listes de campus
+            echo '<div><label for="campus" >Campus : <select name="campus" id="campus">';
+            $campus = Location::getAll();
+            foreach ($campus as $one_campus) {
+                echo "<option value=\"".$one_campus->getId()."\">".$one_campus->getName()."</option>";
+            }
+            echo '</select></label></div>';
 
-        //Listes des employeur
-        echo '<div><label for="employer" >Employeur : </label>
+            //Listes des employeur
+            echo '<div><label for="employer" >Employeur : </label>
                     <select name="employer" id="employer">
                         <option value="sorbonne-universite">Sorbonne université</option>
                         <option value="cnrs">CNRS</option>
                         <option value="autre">Autre</option>
                     </select></div>';
 
-        echo '<div><label for="mailCase" >Case de courrier : </label><input type="text" name="mailCase" id="mailCase" value="'.($user_data->caseCourrier ?? "").'" required></div>';
+            echo '<div><label for="mailCase" >Case de courrier : </label><input type="text" name="mailCase" id="mailCase" value="'.($user_data->getMailCase() ?? "").'" required></div>';
 
 
-        echo '<input type="hidden" name="idUser" value="' . $id_user . '">';
-        echo '<input type="hidden" name="idUserWp" value="' . $user_data->wp_user_id . '">';
-        submit_button('Modifier', 'primary', 'changeTeams');
-        echo "</form>";
-        echo "</div>";
+            echo '<input type="hidden" name="idUser" value="' . $id_user . '">';
+            echo '<input type="hidden" name="idUserWp" value="' . $user_data->getWpId() . '">';
+            submit_button('Modifier', 'primary', 'changeTeams');
+            echo "</form>";
+            echo "</div>";
+        } catch (\MUDF_ISTEP\Exception\InvalidParameter|\MUDF_ISTEP\Exception\MemberNotFound $e) {
+            echo '<div class="notice notice-error"><p>Une erreur est survenue</p></div>';
+        }
+
+
     } else {
         wp_redirect(admin_url("admin.php?page=istep_users_list"));
 
