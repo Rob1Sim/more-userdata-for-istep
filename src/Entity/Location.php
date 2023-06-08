11c525f85bebb4fbd894af80e4dddf67c9beea63
Robin Simonneau <robin.le.dev@gmail.com>
2023-06-08 11:24:35 +0200

    Ajout de la class Location.php

---
+++ src/Entity/Location.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace MUDF_ISTEP\Entity;
+
+use MUDF_ISTEP\Interface\IWpEntity;
+
+class Location implements \MUDF_ISTEP\Interface\IWpEntity
+{
+    private int $id;
+    private string $name;
+
+    /**
+     * @param int $id
+     * @param string $name
+     */
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function findById(int $id): \MUDF_ISTEP\Interface\IWpEntity
+    {
+        // TODO: Implement findById() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getAll(): array
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function createEntityFromWPDB($entity): \MUDF_ISTEP\Interface\IWpEntity
+    {
+        // TODO: Implement createEntityFromWPDB() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    static function getTableName(): string
+    {
+        // TODO: Implement getTableName() method.
+    }
+}
